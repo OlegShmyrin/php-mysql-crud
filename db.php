38fa17269a1030c67d3988d57e5d7402693284a8
Oleg Shmyrin <oleg.shmyrin@gmail.com>
2022-11-22 22:38:05 +0300

    define variables to DB connection

--- db.php
+++ db.php
@@ -1,11 +1,17 @@
 <?php
+
+define("DB_HOST", "db-hostane");
+define("DB_USER", "root");
+define("DB_PASSWORD", "password123");
+define("DB_DATABASE", "php_mysql_crud");
+
 session_start();
 
 $conn = mysqli_connect(
-  '192.168.1.110',
-  'root',
-  'password123',
-  'php_mysql_crud'
+  "DB_HOST",
+  "DB_USER",
+  "DB_PASSWORD",
+  "DB_DATABASE"
 ) or die(mysqli_error($mysqli));
 
 ?>
